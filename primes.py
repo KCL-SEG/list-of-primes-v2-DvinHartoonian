a1d901325aea8b4521a6437fb56572317c98a1d4
DvinHartoonian <dvin.hartoonian@gmail.com>
2022-09-30 14:23:44 +0100

    Add exception for when primes() input is 0 or lower.

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError(f'{number_of_primes} not accepted. Value must be higher than 0.')
+    else:
+        list = []
+        num = 2
+
+        for x in range(number_of_primes):
+            primeFound = False
+
+            while primeFound == False:
+                prime = True
+
+                for i in range(2, num):
+                    if num % i == 0:
+                        prime = False
+
+                if prime:
+                    list.append(num)
+                    primeFound = True
+
+                num += 1
+
     return list
